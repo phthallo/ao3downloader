bc1c600538681978c1a48bade495f8ee38b14523
phthallo <84078890+phthallo@users.noreply.github.com>
2023-05-31 21:17:37 +0930

    Merged work/series/file args + minor changes
    Merged work/series/file arguments under '-source' in preparation for the addition of bookmarks downloads.

--- README.md
+++ README.md
@@ -10,40 +10,44 @@ Extremely basic CLI for downloading fanworks from archiveofourown.org in a varie
 
 ## Instructions for Use
 - Clone the repository. 
-- 
+
 `git clone https://github.com/phthallo/ao3downloader/`
 
 - Run `cmd` in the location of the downloaded file.
 
 ```
-usage: Ao3 Downloader [-h] [-save [SAVE]] [-filetype [{azw3,epub,mobi,pdf,html}]] [--work | --file] path
+usage: Ao3 Downloader [-h] [-save [SAVE]] [-format [{azw3,epub,mobi,pdf,html}]] [-login [LOGIN]]
+                      [-source [{url,file,bookmarks}]]
+                      path
 
-Accepts an Archive of Our Own link, or a .txt file containing Ao3 links, and downloads the work or series as  
-a requested file type in a specified location. Default settings can be viewed in settings.json. Source code   
-can be found here: https://github.com/phthallo/ao3downloader
+Accepts an Archive of Our Own link, or a .txt file containing Ao3 links, and downloads the work or series as a     
+requested file type in a specified location. Default settings can be viewed in settings.json. Source code and      
+example usage can be found here: https://github.com/phthallo/ao3downloader
 
 positional arguments:
-  path                  If downloading a work or series, enter its Ao3 URL. If downloading from a text file,  
-                        enter the location of the text file
+  path                  If downloading a work or series, enter its Ao3 URL. If downloading from a text file,       
+                        enter the location of the text file. If downloading from bookmarks, enter '+'
 
 options:
   -h, --help            show this help message and exit
   -save [SAVE]          Specify a folder to save files in. This will be remembered.
-  -filetype [{azw3,epub,mobi,pdf,html}]
-                        Specify a file type out of one of the following: azw3, epub, mobi, pdf, html. This    
-                        will be remembered.
-  --work                Specify to download a singular work or series.
-  --file                Specify to download from a text file.
+  -format [{azw3,epub,mobi,pdf,html}]
+                        Specify a file type out of one of the following: azw3, epub, mobi, pdf, html. This will    
+                        be remembered.
+  -login [LOGIN]        Specify your Ao3 login to download fanworks from your bookmarks, in the following format:  
+                        username:password. This will be remembered.
+  -source [{url,file,bookmarks}]
+                        Specify to download from an AO3 work/series URL, from a file, or from your bookmarks.   
  ```
   
 ## Example usage
 ### Downloading a singular fanwork
-`python ao3download.py -filetype pdf --work https://archiveofourown.org/works/17400464`
+`python ao3download.py -format mobi -source url https://archiveofourown.org/works/17400464`
 
-This will save the fanfiction as `Stag Beetles and Broken Legs.pdf` at the following path: `C:/Users/USERNAME/Documents/Archive of Our Own/Works`. 
+This will save the fanfiction as `Stag Beetles and Broken Legs.mobi` at the following path: `C:/Users/USERNAME/Documents/Archive of Our Own/Works`. 
 
 ### Downlading a series
-`python ao3download.py -filetype pdf --work https://archiveofourown.org/series/1264421`
+`python ao3download.py -format pdf -source url https://archiveofourown.org/series/1264421`
 
 This will save the series as separate `.pdf` files for each work at the following path: `C:/Users/%username%/Documents/Archive of Our Own/Stag Beetles and Broken Legs`. 
 
@@ -57,9 +61,9 @@ https://archiveofourown.org/works/17623268
 ```
 where all works are separated by newlines.
 
-`python ao3download.py --file C:/Users/USERNAME/foo/bar/path/textfile.txt`
+`python ao3download.py -source file C:/Users/USERNAME/foo/bar/path/textfile.txt`
 
-This will open the specified text file and save all files as `.pdf` files in the `/Works` folder. 
+This will open the specified text file and save all the links inside as `.pdf` files in the `/Works` folder. 
 If there are series located in this text file as well, they will be saved at `Archive of Our Own\<series name>\` instead.
 
 ## Changing default settings
@@ -68,6 +72,9 @@ You can update the default settings for save location and filetype by using the 
 `-save [SAVE]` [default is C:/Users/USERNAME/Documents/Archive of Our Own]
 `-filetype [{azw3,epub,mobi,pdf,html}]` [default is PDF]
 
+## Saving from your bookmarks
+Will be added fully later. Please note that your login is stored within the `.json` file so that it persists across usages; do not share this file with others. I will add an option to forget this later. 
+
 ## To-Do List
 - [x] Allow user to customise locations of saved files.
 
@@ -75,9 +82,9 @@ You can update the default settings for save location and filetype by using the 
 
 - [ ] Add author names to be added to file names.
 
-- [ ] Allow user to search the archive and download from there.
+- [ ] Allow user to search the archive and download from there. 
 
-- [ ] Merge separated --file/--work arguments.
+- [x] Merge separated --file/--work arguments.
 
-- [ ] ...make the entire thing neater
+- [ ] ...refactoring everything 
 
--- ao3download.py
+++ ao3download.py
@@ -1,3 +1,11 @@
+# TO DO 
+# Merge -work/series and -file argument to instead be a parameter which accepts three arguments (url/file/bookmarks)
+# [x] Argument merged, just need to fix up everything else.
+# Add users login functionality to save works from bookmarks 
+# [x] Allow user to enter login IT'S STORED IN PLAINTEXT DON'T SHARE YOUR FILES
+# Update readme
+# [X] Fixed inconsistency with apostrophes in series and work downloads
+
 import AO3
 import os.path
 import argparse
@@ -8,43 +16,55 @@ with open('settings.json') as json_file:
     settings = json.load(json_file)
     json_file.close()
 save = settings["save_location"]
-filetype = settings["file_type"]
+format = settings["format"]
+login = settings["login"]
 
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(prog="Ao3 Downloader", 
-                                 description="Accepts an Archive of Our Own link, or a .txt file containing Ao3 links, and downloads the work or series as a requested file type in a specified location. Default settings can be viewed in settings.json. Source code can be found here: https://github.com/phthallo/ao3downloader")
+                                 description="Accepts an Archive of Our Own link, or a .txt file containing Ao3 links, and downloads the work or series as a requested file type in a specified location. Default settings can be viewed in settings.json. Source code and example usage can be found here: https://github.com/phthallo/ao3downloader")
 # save file location and file type
 parser.add_argument('-save', nargs = "?", help="Specify a folder to save files in. This will be remembered.", default=save)
-parser.add_argument('-filetype', nargs = "?", choices = [
+parser.add_argument('-format', nargs = "?", choices = [
     "azw3", 
     "epub", 
     "mobi", 
     "pdf", 
-    "html"], help="Specify a file type out of one of the following: \nazw3, epub, mobi, pdf, html. This will be remembered.", default=filetype)
-
+    "html"], help="Specify a file type out of one of the following: \nazw3, epub, mobi, pdf, html. This will be remembered.", default=format)
+parser.add_argument('-login', nargs="?", help="Specify your Ao3 login to download fanworks from your bookmarks, in the following format: username:password. This will be remembered.", default=login)
 #### Questions if it's a work or series.
-group = parser.add_mutually_exclusive_group()
-group.add_argument('--work', action='store_true',  help="Specify to download a singular work or series.")
-group.add_argument('--file', action='store_true', help="Specify to download from a text file.")
+parser.add_argument('-source', nargs = "?", choices = [
+    "url",
+    "file",
+    "bookmarks"], help="Specify to download from an AO3 work/series URL, from a file, or from your bookmarks.")
 
 #### Location of fics to be downloaded. Not optional.
-parser.add_argument('path', type=str, help="If downloading a work or series, enter its Ao3 URL. \nIf downloading from a text file, enter the location of the text file")
+parser.add_argument('path', type=str, help="If downloading a work or series, enter its Ao3 URL. If downloading from a text file, enter the location of the text file. If downloading from bookmarks, enter '+'")
+
+#### Argument parsing
 args = parser.parse_args()
+source = args.source 
+format = args.format
+if args.login == ["DEFAULT", "DEFAULT"] and source == "bookmarks": #if the person is trying to access their bookmarks but they haven't set a username and password, prompt them to.
+    print("Before using this command, set your login using `python ao3download.py -login yourusername:yourpassword`")
+    quit()
+elif args.login != ["DEFAULT", "DEFAULT"]: #if the person is just trying to set their login, regex the : out and makes it a list.
+    login = (re.sub(":", " ", args.login, 1)).split() #splits input into username and password based on the location of the first semicolon. ao3 can't have semicolons in the username, so this should work.
+    print("Login credentials recorded.")
 path = args.path
-if save == "C:/Users/USERNAME/Documents/Archive of Our Own": #if the save location is the default
-    save = f"C:/Users/{os.getlogin()}/Documents/Archive of Our Own" #replace it with the actual username.
+if save == "C:/Users/DEFAULT/Documents/Archive of Our Own": #if the save location is the default
+    save = f"C:/Users/{os.getlogin()}/Documents/Archive of Our Own" #replace it with the actual username of the profile. Originally set as %username%, but that was interpreted literally and started creating files in the Users folder.
 else:
-     save = args.save
-filetype = (args.filetype).lower()
+     save = args.save 
 
 # dump settings.
 with open('settings.json', "w") as json_file:
     settings = {"save_location": save,
-                "file_type": filetype}
+                "format": format,
+                "login": login}
     json.dump(settings, json_file)
     json_file.close()
 
-#retrieve two things: work/series status and id of the given link.
+#retrieve two things: work/series status and id of the given link and returns as list. 
 def idSearch(link):
     id = re.findall(r'[A-Za-z]+/[0-9]+', link)
     id = ' '.join(id).replace("/", " " ).split()
@@ -69,9 +89,9 @@ def workDownload(workLink):
     filesave = f'{save}/Works'
     workTitle = stripPunctuation(workLink.title)
     createFolder(filesave)
-    with open(os.path.join(filesave, workTitle + '.' + filetype), "wb") as file:
-        file.write(workLink.download(filetype))
-    print(f"{workLink.title} was downloaded.")
+    with open(os.path.join(filesave, workTitle + '.' + format), "wb") as file:
+        file.write(workLink.download(format))
+    print(f"'{workLink.title}' was downloaded.")
 
 #### SERIES DOWNLOAD CODE ####
 def seriesDownload(seriesLink):
@@ -88,12 +108,12 @@ def seriesDownload(seriesLink):
     print(f"// Commencing the download of {seriesLink.name}.")
     for work in seriesworks:
         workTitle = stripPunctuation(work.title)
-        with open(str(os.path.join(filesave, workTitle + '.'+ filetype)), "wb") as file:
-            file.write(work.download(filetype))
+        with open(str(os.path.join(filesave, workTitle + '.'+ format)), "wb") as file:
+            file.write(work.download(format))
             print(f"{work.title} was downloaded. {seriesworks.index(work)+1} out of {len(seriesworks)} series downloads complete.")
     print(f"// The series '{seriesLink.name}' was downloaded.")
 
-if args.work:
+if source == "url":
     workDescriptor = idSearch(path)[0]
     workID = idSearch(path)[1]
     if workDescriptor == "works":
@@ -101,9 +121,9 @@ if args.work:
     elif workDescriptor == "series":
         seriesDownload(workID)
     else:
-        print("Something went wrong. Did you mean to enter in --file instead?")
-
-if args.file:
+        print("Something went wrong.")
+        
+elif source == "file":
     with open(path, 'r') as ficfile:
         lines = ficfile.readlines()
     for i in lines:
@@ -114,5 +134,7 @@ if args.file:
         elif workDescriptor == "series":
             seriesDownload(workID)
         else:
-            print("Something went wrong. \nPlease make sure your links are in the right format, e.g https://archiveofourown.org/<works or series>/<id> ")
-         
+            print("Something went wrong. \nPlease make sure your links are in the right format, e.g https://archiveofourown.org/<works or series>/<id>.")
+
+if source == "bookmarks" and path == "+":
+    print("Bookmarks downloads coming soon.")
